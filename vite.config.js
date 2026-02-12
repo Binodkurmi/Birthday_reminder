e317773dc25a9e6e94c5ce8716cfb72c45d545b7
Binod Chaudhary <binodchaudhary3850@gmail.com>
2026-02-12 19:25:26 +0530

    Added PWA service worker registration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-        tailwindcss(),
-  ],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BirthIQ Birthday Reminder',
+        short_name: 'BirthIQ',
+        description: 'Smart Birthday Reminder App',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
